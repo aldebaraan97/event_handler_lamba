64204a1a0f51d90a4814803189b1b446e6f3d4c8
Aldebaraan <aldecanedo97@gmail.com>
2024-10-31 16:29:22 -0400

    Event handlers defined using lambda expressions

--- main/java/com/example/event_handling_lambada/HelloApplication.java
+++ main/java/com/example/event_handling_lambada/HelloApplication.java
@@ -20,6 +20,36 @@ public class HelloApplication extends Application {
         Text text = new Text(40, 40, "Programming is fun");
         Pane pane = new Pane(text);
 
-        // Hold
+        // Hold four buttons in an HBox
+        Button btUp = new Button("Up");
+        Button btDown = new Button("Down");
+        Button btLeft = new Button("Left");
+        Button btRight = new Button("Right");
+        HBox hBox = new HBox(btUp, btDown, btLeft, btRight);
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setBottom(hBox);
+
+        //Create and register the handler
+        // Note that different notations are used, they're all equivalent
+        btUp.setOnAction((ActionEvent e) -> {
+            text.setY(text.getY() > 10 ? text.getY() - 5 : 10);
+        });
+
+        btDown.setOnAction((e) -> {
+            text.setY(text.getY() < pane.getHeight() ?
+                    text.getY() + 5 : pane.getHeight() + 5);
+        });
+
+        btLeft.setOnAction(e -> {
+            text.setX(text.getX() > 0 ? text.getX() - 5 : 0);
+        });
+
+        btRight.setOnAction(e ->
+                text.setX(text.getX() < pane.getWidth() - 100 ?
+                        text.getX() + 5 : pane.getWidth() - 100)
+        );
     }
 }
